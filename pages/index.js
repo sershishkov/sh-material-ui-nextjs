3f893ad6dacc6ec2533d6807188d023296d111e5
sershishkov <ser.shishkov@gmail.com>
2021-01-09 11:48:14 +0200

    15-186

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,95 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import AddIcon from '@material-ui/icons/Add';
+
+import Switch from '@material-ui/core/Switch';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 const useStyles = makeStyles((theme) => ({}));
 
 const Index = () => {
   const classes = useStyles();
+  const [websiteChecked, setWebsiteChecked] = useState(false);
+  const [iOSChecked, setiOSChecked] = useState(false);
+  const [androidChecked, setAndroidChecked] = useState(false);
+  const [softwareChecked, setSofwareChecked] = useState(false);
+
   return (
     <Grid container direction='column'>
-      <Grid item>
+      <Grid item style={{ marginTop: '2em', marginLeft: '5em' }}>
         <Typography variant='h1'>Progects</Typography>
+      </Grid>
+      <Grid item>
+        <TextField
+          placeholder='Search project details or create a new entry.'
+          style={{ width: '35em', marginLeft: '5em' }}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position='end'>
+                <AddIcon color='primary' />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Grid>
+      <Grid item style={{ marginLeft: '5em', marginTop: '2em' }}>
+        <FormGroup row>
+          <FormControlLabel
+            style={{ marginRight: '5em' }}
+            control={
+              <Switch
+                checked={websiteChecked}
+                color='primary'
+                onChange={() => setWebsiteChecked(!websiteChecked)}
+              />
+            }
+            label='Websites'
+            labelPlacement='start'
+          />
+
+          <FormControlLabel
+            style={{ marginRight: '5em' }}
+            control={
+              <Switch
+                checked={iOSChecked}
+                color='primary'
+                onChange={() => setiOSChecked(!iOSChecked)}
+              />
+            }
+            label='iOS Apps'
+            labelPlacement='start'
+          />
+
+          <FormControlLabel
+            style={{ marginRight: '5em' }}
+            control={
+              <Switch
+                checked={androidChecked}
+                color='primary'
+                onChange={() => setAndroidChecked(!androidChecked)}
+              />
+            }
+            label='Android'
+            labelPlacement='start'
+          />
+
+          <FormControlLabel
+            control={
+              <Switch
+                checked={softwareChecked}
+                color='primary'
+                onChange={() => setSofwareChecked(!softwareChecked)}
+              />
+            }
+            label='Custom Software'
+            labelPlacement='start'
+          />
+        </FormGroup>
       </Grid>
     </Grid>
   );
--- src/ui/theme.js
+++ src/ui/theme.js
@@ -1,109 +1,118 @@
-import { createMuiTheme } from "@material-ui/core/styles";
+import { createMuiTheme } from '@material-ui/core/styles';
 
-const arcBlue = "#0B72B9";
-const arcOrange = "#FFBA60";
-const arcGrey = "#868686";
+const arcBlue = '#0B72B9';
+const arcOrange = '#FFBA60';
+const arcGrey = '#868686';
 
 export default createMuiTheme({
   palette: {
     common: {
       blue: arcBlue,
-      orange: arcOrange
+      orange: arcOrange,
     },
     primary: {
-      main: arcBlue
+      main: arcBlue,
     },
     secondary: {
-      main: arcOrange
-    }
+      main: arcOrange,
+    },
   },
   typography: {
     tab: {
-      fontFamily: "Raleway",
-      textTransform: "none",
+      fontFamily: 'Raleway',
+      textTransform: 'none',
       fontWeight: 700,
-      color: "white",
-      fontSize: "1rem"
+      color: 'white',
+      fontSize: '1rem',
     },
     estimate: {
-      fontFamily: "Pacifico",
-      fontSize: "1rem",
-      textTransform: "none",
-      color: "white"
+      fontFamily: 'Pacifico',
+      fontSize: '1rem',
+      textTransform: 'none',
+      color: 'white',
     },
     h1: {
-      fontFamily: "Raleway",
+      fontFamily: 'Raleway',
       fontWeight: 700,
-      fontSize: "2.5rem",
+      fontSize: '2.5rem',
       color: arcBlue,
-      lineHeight: 1.5
+      lineHeight: 1.5,
     },
     h3: {
-      fontFamily: "Pacifico",
-      fontSize: "2.5rem",
-      color: arcBlue
+      fontFamily: 'Pacifico',
+      fontSize: '2.5rem',
+      color: arcBlue,
     },
     h4: {
-      fontFamily: "Raleway",
-      fontSize: "1.75rem",
+      fontFamily: 'Raleway',
+      fontSize: '1.75rem',
       color: arcBlue,
-      fontWeight: 700
+      fontWeight: 700,
     },
     h6: {
       fontWeight: 500,
-      fontFamily: "Raleway",
-      color: arcBlue
+      fontFamily: 'Raleway',
+      color: arcBlue,
     },
     subtitle1: {
-      fontSize: "1.25rem",
+      fontSize: '1.25rem',
       fontWeight: 300,
-      color: arcGrey
+      color: arcGrey,
     },
     subtitle2: {
-      color: "white",
+      color: 'white',
       fontWeight: 300,
-      fontSize: "1.25rem"
+      fontSize: '1.25rem',
     },
     body1: {
-      fontSize: "1.25rem",
+      fontSize: '1.25rem',
       color: arcGrey,
-      fontWeight: 300
+      fontWeight: 300,
     },
     caption: {
-      fontSize: "1rem",
+      fontSize: '1rem',
       fontWeight: 300,
-      color: arcGrey
+      color: arcGrey,
     },
     learnButton: {
       borderColor: arcBlue,
       borderWidth: 2,
-      textTransform: "none",
+      textTransform: 'none',
       color: arcBlue,
       borderRadius: 50,
-      fontFamily: "Roboto",
-      fontWeight: "bold"
-    }
+      fontFamily: 'Roboto',
+      fontWeight: 'bold',
+    },
   },
   overrides: {
+    MuiFormControlLabel: {
+      label: {
+        color: arcBlue,
+        fontWeight: 700,
+      },
+      labelPlacementStart: {
+        marginLeft: 0,
+      },
+    },
     MuiInputLabel: {
       root: {
         color: arcBlue,
-        fontSize: "1rem"
-      }
+        fontSize: '1rem',
+      },
     },
     MuiInput: {
       root: {
         color: arcGrey,
-        fontWeight: 300
+        fontWeight: 300,
       },
       underline: {
-        "&:before": {
-          borderBottom: `2px solid ${arcBlue}`
+        '&:before': {
+          borderBottom: `2px solid ${arcBlue}`,
         },
-        "&:hover:not($disabled):not($focused):not($error):before": {
-          borderBottom: `2px solid ${arcBlue}`
-        }
-      }
-    }
-  }
+        '&:hover:not($disabled):not($focused):not($error):before': {
+          borderBottom: `2px solid ${arcBlue}`,
+        },
+      },
+    },
+  },
 });
